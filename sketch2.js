59b7e6a3514d87db4137987903851c1bc2ec0422
bigboybryson <52322344+bigboybryson@users.noreply.github.com>
2020-01-29 17:43:22 -0800

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -8,4 +8,19 @@ break;
      window.location.href = "https://bigboybryson.github.io/theplanegame/index3.html"
     break;
 
+      default:
+ alert("bot or normal")
+    var web = prompt("What's version of the plane game do you want to play,bot or normal");
+switch(web) {
+  case "normal":
+   window.location.href = "https://bigboybryson.github.io/theplanegame/index2.html";
+break;
+
+  case "bot":
+     window.location.href = "https://bigboybryson.github.io/theplanegame/index3.html"
+    break;
 }
+
+   
+}
+
