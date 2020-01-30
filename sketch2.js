93ed40237f109fed736a8b8338c2bef659247dd3
bigboybryson <52322344+bigboybryson@users.noreply.github.com>
2020-01-29 17:33:56 -0800

    Add files via upload

---
+++ sketch2.js
@@ -0,0 +1,11 @@
+var web = prompt("What's version of the plane game do you want to play,bot or normal");
+switch(web) {
+  case "normal":
+   window.location.href = "https://bigboybryson.github.io/theplanegame/index2.html";
+break;
+
+  case "bot":
+     window.location.href = "https://troydatboi134.github.io/theplanegame/index2.html"
+    break;
+
+}
