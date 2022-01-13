46656c7ae5afb06b4afdaa40d9b89543e2f79f4c
Troy Petty <58780638+troydatboi134@users.noreply.github.com>
2022-01-13 10:07:53 -0600

    Create sketch.js

--- sketch.js
+++ sketch.js
@@ -44,8 +44,11 @@ function draw() {
        mySound.play();
      sprite.x=0;
          if (score=15){coincollect=coincollect+20}
+         
          if (score<15) {if (score<20) {coincollect=coincollect}}
          if (score=20) {coincollect=coincollect+20}
+         if (score>15){coincollect=coincollect-20}
+         if (score>20 {coincollect=coincollect-20
    coincollect=coincollect+.0625;
              score = score+1;
  sprite2.x=random(400,900);
