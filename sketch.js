0a4321616ef02374ff3fae0a2a44aa5bf5329761
Troy Petty <58780638+troydatboi134@users.noreply.github.com>
2022-01-14 07:18:45 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,52 +23,6 @@ group.add(sprite);
 function draw() {
  if (sprite.x===sprite.x) {
     sprite.y = mouseY;
-    sprite.y = sprite2.y;
-    sprite.x = sprite.x+coincollect;
-  }
- time=time+1
-  textSize(40);
-  sprite.scale=.50;
-   if (sprite.x>900){
-             sprite.y =200;
-              coincollect=6;
-           time=0;
-           sprite.x=50
-           score=0
-         
-                 sprite2.y=random(100,290);
-               }
-   background("blue");
-  if (group.isTouching(sprite2)) {
-       mySound.play();
-     sprite.x=0;
-         if (score>20){if (score<30) {coincollect=coincollect+.25}}
-         function preload(){
-  mySound = loadSound('ppe.mp3');
-
-}
-function setup(){  
-    createCanvas(900,400);
-  time = 0;
-  sprite2=createSprite(450,200,0,0);
-img=loadImage("e.png")
-sprite2.addImage(img);
-sprite2.scale=0.5
-  sprite = createSprite(10, 200,0,0);
-img=loadImage("s.png")
-sprite.addImage(img);
-  group = createGroup();
-group.add(sprite);
-  score = 0 ;
-  pause = 0;
-  pause2 = .5; 
-  coincollect = 6;
-  time2 = 0;
-}
-function draw() {
- if (sprite.x===sprite.x) {
-    sprite.y = mouseY;
-    sprite.y = sprite2.y;
     sprite.x = sprite.x+coincollect;
   }
  time=time+1
@@ -81,28 +35,16 @@ function draw() {
            sprite.x=50
            score=0
             
-            sprite2.y=random(100,290);
+                 sprite2.y=random(100,290);
                }
    background("blue");
   if (group.isTouching(sprite2)) {
        mySound.play();
      sprite.x=0;
-         if (coincollect>40){coincollect=40}
-         coincollect=coincollect+.5;
-             score = score+1;
- sprite2.x=random(400,900);
-      sprite2.y=random(20,370); }
-    fill("yellow");
-   text('Score',400,300 );
-   fill("red");
-   text(score,450,342 );
-    fill("red");
-    drawSprites(); 
-  }if (score>40){if (score<50){coincollect=coincollect+.125}}
-         if (score>60){coincollect=coincollect+.625}
+         
    coincollect=coincollect+.5;
              score = score+1;
- sprite2.x=random(400,900);
+ 
       sprite2.y=random(20,370); }
     fill("yellow");
    text('Score',400,300 );
